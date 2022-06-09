2e2fdeaa17582bf75734de8509c632fc83b3f6bb
Cristian Hernández <chtello05@gmail.com>
2022-06-08 23:13:38 -0600

    primer diseno html

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.myreviews').carousel({
+        numVisible:7,
+        shift:55,
+        padding:55,
+    });
+    $('.slider' ).slider({full_width: true});
+    });
